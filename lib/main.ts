62e650fb348758a6eed03bc4c636964022e6885e
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-11-02 14:26:37 +0900

    :construction: wip: implement base #6

--- lib/main.ts
+++ lib/main.ts
@@ -16,7 +16,34 @@ class RatingStar {
 
     console.log(filledStar + emptyStar);
   }
+
+  getRatingIcon(): string {
+    const filledStarIcon = '★';
+    const emptyStarIcon = '☆';
+
+    const filledStar = filledStarIcon.repeat(this.givenStars);
+    const emptyStar = emptyStarIcon.repeat(this.totalStars - this.givenStars);
+
+    return filledStar + emptyStar;
+  }
 }
 
 const rating = new RatingStar(4, 5);
 rating.displayStar();
+
+const ratingStarDoms = document.querySelectorAll('.rs');
+
+console.log(ratingStarDoms);
+
+for (const dom of ratingStarDoms) {
+  if (dom.textContent !== null) {
+    const [givenStars, totalStars]: number[] = dom.textContent.split('/').map(Number);
+    console.log(givenStars, totalStars);
+    if (!givenStars || !totalStars) {
+      console.error('数値が入力されていません 3/5 のような形で指定してください:', dom);
+    }
+
+    const rating = new RatingStar(givenStars, totalStars);
+    dom.textContent = rating.getRatingIcon();
+  }
+}
