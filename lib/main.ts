b399cd18f74c424bff1fde5c05a4c87564642b4d
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-11-02 16:28:58 +0900

    :construction: wip: ★と☆、色分けが完成 #10

--- lib/main.ts
+++ lib/main.ts
@@ -1,10 +1,13 @@
 class Star {
   givenStars: number;
   totalStars: number;
+  starDoms: HTMLElement[];
   constructor(givenStars: number, totalStars: number) {
     // totalStars 点中、givenStart 点
     this.givenStars = givenStars;
     this.totalStars = totalStars;
+    this.starDoms = [];
+    this.wrapAllStars();
   }
 
   displayStar(): void {
@@ -21,10 +24,49 @@ class Star {
     const filledStarIcon = '★';
     const emptyStarIcon = '☆';
 
-    const filledStar = filledStarIcon.repeat(this.givenStars);
-    const emptyStar = emptyStarIcon.repeat(this.totalStars - this.givenStars);
+    const filledStar = filledStarIcon.repeat(Math.ceil(this.givenStars));
+    const emptyStar = emptyStarIcon.repeat(this.totalStars - Math.ceil(this.givenStars));
 
     return filledStar + emptyStar;
+  }
+
+  wrapAllStars() {
+    const stars = this.getRatingIcon().split('');
+
+    for (let i = 0; i < stars.length; i++) {
+      const wrappedStar = this.wrapWithSpan(stars[i], i);
+      this.starDoms.push(wrappedStar);
+    }
+  }
+
+  wrapWithSpan(value: string, index: number) {
+    const span = document.createElement('span');
+    span.textContent = value;
+    if (isDecimal(this.givenStars)) {
+      // ここで小数点以下が必要になる
+      // 0.1の1の部分が範囲されるようにしなくてはいけない
+      if (Math.floor(this.givenStars) === index) {
+        span.classList.add('rs-control');
+        const number = ((this.givenStars % 1) * 10).toFixed(0);
+        span.classList.add(`rs-${number}`);
+      }
+      if (Math.floor(this.givenStars) < index) {
+        span.classList.add('rs-empty');
+      }
+    }
+
+    if (this.givenStars <= index) {
+      span.classList.add('rs-empty');
+    }
+
+    return span;
+  }
+
+  appendSpan(parentDom: Element) {
+    parentDom.textContent = '';
+    this.starDoms.map((dom) => {
+      parentDom.appendChild(dom);
+    });
   }
 }
 
@@ -58,7 +100,7 @@ export class RaitingStar {
         }
 
         const rating = new Star(givenStars, totalStars);
-        dom.textContent = rating.getRatingIcon();
+        rating.appendSpan(dom);
       }
     }
   }
