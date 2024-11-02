5ea51d034c362ae30f29a38b2001fc43ff9bed94
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-11-02 17:05:27 +0900

    :crystal_ball: refactor: separate files #12 (#14)
    * :construction: wip: クラス、ユーティルで分割 #12
    
    * :construction: wip: add some memo #12

---
+++ lib/Star.ts
@@ -0,0 +1,81 @@
+import { isDecimal } from './utils';
+
+// もしかしてここは
+// Starクラス作って、
+// Emptyクラス
+// Fillクラス
+// 間クラス
+// を作ってわけられるのではないか？...とりあえず保留
+
+export default class Star {
+  givenStars: number;
+  totalStars: number;
+  starDoms: HTMLElement[];
+  constructor(givenStars: number, totalStars: number) {
+    // totalStars 点中、givenStars 点
+    // ( 4 / 5 ) = ( givenStars / totalStars )
+    this.givenStars = givenStars;
+    this.totalStars = totalStars;
+    this.starDoms = [];
+    this.wrapAllStars();
+  }
+
+  displayStar(): void {
+    const filledStarIcon = '★';
+    const emptyStarIcon = '☆';
+
+    const filledStar = filledStarIcon.repeat(this.givenStars);
+    const emptyStar = emptyStarIcon.repeat(this.totalStars - this.givenStars);
+
+    console.log(filledStar + emptyStar);
+  }
+
+  getRatingIcon(): string {
+    const filledStarIcon = '★';
+    const emptyStarIcon = '☆';
+
+    const filledStar = filledStarIcon.repeat(Math.ceil(this.givenStars));
+    const emptyStar = emptyStarIcon.repeat(this.totalStars - Math.ceil(this.givenStars));
+
+    return filledStar + emptyStar;
+  }
+
+  wrapAllStars() {
+    const stars = this.getRatingIcon().split('');
+
+    for (let i = 0; i < stars.length; i++) {
+      const wrappedStar = this.wrapWithSpan(stars[i], i);
+      this.starDoms.push(wrappedStar);
+    }
+  }
+
+  wrapWithSpan(value: string, index: number) {
+    const span = document.createElement('span');
+    span.textContent = value;
+    if (isDecimal(this.givenStars)) {
+      // ここで小数点以下が必要になる
+      // 0.1の1の部分が範囲されるようにしなくてはいけない
+      if (Math.floor(this.givenStars) === index) {
+        span.classList.add('rs-control');
+        const number = ((this.givenStars % 1) * 10).toFixed(0);
+        span.classList.add(`rs-${number}`);
+      }
+      if (Math.floor(this.givenStars) < index) {
+        span.classList.add('rs-empty');
+      }
+    }
+
+    if (this.givenStars <= index) {
+      span.classList.add('rs-empty');
+    }
+
+    return span;
+  }
+
+  appendSpan(parentDom: Element) {
+    parentDom.textContent = '';
+    this.starDoms.map((dom) => {
+      parentDom.appendChild(dom);
+    });
+  }
+}
--- lib/main.ts
+++ lib/main.ts
@@ -1,82 +1,12 @@
 import './style.css';
+import Star from './Star';
+import { isDecimal, customRoundToFirstDecimalPrace } from './utils';
 
-class Star {
-  givenStars: number;
-  totalStars: number;
-  starDoms: HTMLElement[];
-  constructor(givenStars: number, totalStars: number) {
-    // totalStars 点中、givenStart 点
-    this.givenStars = givenStars;
-    this.totalStars = totalStars;
-    this.starDoms = [];
-    this.wrapAllStars();
-  }
-
-  displayStar(): void {
-    const filledStarIcon = '★';
-    const emptyStarIcon = '☆';
-
-    const filledStar = filledStarIcon.repeat(this.givenStars);
-    const emptyStar = emptyStarIcon.repeat(this.totalStars - this.givenStars);
-
-    console.log(filledStar + emptyStar);
-  }
-
-  getRatingIcon(): string {
-    const filledStarIcon = '★';
-    const emptyStarIcon = '☆';
-
-    const filledStar = filledStarIcon.repeat(Math.ceil(this.givenStars));
-    const emptyStar = emptyStarIcon.repeat(this.totalStars - Math.ceil(this.givenStars));
-
-    return filledStar + emptyStar;
-  }
-
-  wrapAllStars() {
-    const stars = this.getRatingIcon().split('');
-
-    for (let i = 0; i < stars.length; i++) {
-      const wrappedStar = this.wrapWithSpan(stars[i], i);
-      this.starDoms.push(wrappedStar);
-    }
-  }
-
-  wrapWithSpan(value: string, index: number) {
-    const span = document.createElement('span');
-    span.textContent = value;
-    if (isDecimal(this.givenStars)) {
-      // ここで小数点以下が必要になる
-      // 0.1の1の部分が範囲されるようにしなくてはいけない
-      if (Math.floor(this.givenStars) === index) {
-        span.classList.add('rs-control');
-        const number = ((this.givenStars % 1) * 10).toFixed(0);
-        span.classList.add(`rs-${number}`);
-      }
-      if (Math.floor(this.givenStars) < index) {
-        span.classList.add('rs-empty');
-      }
-    }
-
-    if (this.givenStars <= index) {
-      span.classList.add('rs-empty');
-    }
-
-    return span;
-  }
-
-  appendSpan(parentDom: Element) {
-    parentDom.textContent = '';
-    this.starDoms.map((dom) => {
-      parentDom.appendChild(dom);
-    });
-  }
-}
-
-// const rating = new RatingStar(4, 5);
-// rating.displayStar();
-
-// 名前はあとで、以下と上を変えること
 export class RaitingStar {
+  /*
+    HTML の rs の class属性を持つ要素を取得
+    テキストを解析し、それに応じた結果を返す
+  */
   constructor() {
     const ratingStarDoms = document.querySelectorAll('.rs');
 
@@ -106,14 +36,4 @@ export class RaitingStar {
       }
     }
   }
-}
-
-function isDecimal(number: number) {
-  return !Number.isInteger(number);
-}
-
-function customRoundToFirstDecimalPrace(number: number) {
-  // 小数点第二位を四捨五入
-  const roundedNumber = Math.round(number * 10) / 10;
-  return parseFloat(roundedNumber.toFixed(1));
 }
