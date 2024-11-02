42f02ce388ce53300c25b199b8b899b1d440c6f6
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-11-02 14:09:05 +0900

    :cherry_blossom: feat: console.log(★★★★☆) #5

--- lib/main.ts
+++ lib/main.ts
@@ -1,1 +1,22 @@
-console.log('wa-nn');
+class RatingStar {
+  givenStars: number;
+  totalStars: number;
+  constructor(givenStars: number, totalStars: number) {
+    // totalStars 点中、givenStart 点
+    this.givenStars = givenStars;
+    this.totalStars = totalStars;
+  }
+
+  displayStar(): void {
+    const filledStarIcon = '★';
+    const emptyStarIcon = '☆';
+
+    const filledStar = filledStarIcon.repeat(this.givenStars);
+    const emptyStar = emptyStarIcon.repeat(this.totalStars - this.givenStars);
+
+    console.log(filledStar + emptyStar);
+  }
+}
+
+const rating = new RatingStar(4, 5);
+rating.displayStar();
