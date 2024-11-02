7ba04e535372335660a9d05b58743c2fee763f16
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-11-02 14:09:48 +0900

    :cherry_blossom: feat: console.log(★★★★☆) #5 (#7)
    * :construction: wip: remove unnecessary code #5
    
    * :cherry_blossom: feat: console.log(★★★★☆) #5

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,22 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+class RatingStar {
+  givenStars: number;
+  totalStars: number;
+  constructor(givenStars: number, totalStars: number) {
+    // totalStars 点中、givenStart 点
+    this.givenStars = givenStars;
+    this.totalStars = totalStars;
   }
-  element.addEventListener('click', () => setCounter(++counter))
-  setCounter(0)
+
+  displayStar(): void {
+    const filledStarIcon = '★';
+    const emptyStarIcon = '☆';
+
+    const filledStar = filledStarIcon.repeat(this.givenStars);
+    const emptyStar = emptyStarIcon.repeat(this.totalStars - this.givenStars);
+
+    console.log(filledStar + emptyStar);
+  }
 }
+
+const rating = new RatingStar(4, 5);
+rating.displayStar();
